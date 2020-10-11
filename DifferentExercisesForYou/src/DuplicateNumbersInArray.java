9307ea502c3273069355f198c18b08e3987b397f
Brandio7 <citozibrand@gmail.com>
2020-10-11 10:03:54 +0200

    DuplicateNumbersInArray

---
+++ DifferentExercisesForYou/src/DuplicateNumbersInArray.java
@@ -0,0 +1,24 @@
+//Brand Citozi SDA Academy
+import java.util.Scanner;
+public class DuplicateNumbersInArray {
+    public static void main(String[] args) {
+
+
+            int[] arr = {1,1,2,2,4};
+
+            for (int i = 0; i < arr.length; i++) {
+
+                for (int j = i + 1; j < arr.length; j++) {
+
+                    if (arr[i] == arr[j]) {
+                        System.out.println("Duplicate Element: " + arr[j]);
+
+                    }
+                }
+            }
+
+
+        }
+    }
+
+
